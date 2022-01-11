0e6662e671e0d10682d2b558406d3d197758b82b
Marc St Pierre <marcstpierre93@gmail.com>
2022-01-11 15:03:46 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,21 @@
 ls = [0, 5, 6, 1, 3, 5]
-use_random_seed 6
-#the max value in the choose parameter times
-#the number of items in the list minus 1
-#cannot be greater than the number of items in the list
+mode = (scale :Eb4, :aeolian)
+use_synth :piano
 
 
 define :drift do |seed|
   total = ls.length
   sleeps = []
   (ls.length - 1).times do
-    val = quantise(Math.sin(seed) + 1.125, 0.25)
+    val = quantise(Math.sin( Math::PI * seed / 2) + 1.125, 0.125)
+    rem = quantise((ls.length - sleeps.sum)/2, 0.125)
     if total - val > 0
       sleeps.append(val)
       total -= val
       seed += 1
-      puts total
     else
-      sleeps.append(quantise((ls.length - sleeps.sum)/2, 0.125))
-      total -= quantise((ls.length - sleeps.sum)/2, 0.125)
-      puts total
+      sleeps.append(rem)
+      total -= rem
     end
   end
   sleeps.append(ls.length - sleeps.sum)
@@ -27,11 +24,8 @@ end
 
 live_loop :this do
   rests = drift(tick(:one))
-  atck = drift(tick(:one))
-  rels = drift(tick(:one))
   ls.length.times do
-    use_synth :piano
-    play (scale :Eb4, :aeolian)[ls.tick(:two)], release: rels.look(:two)
+    play mode[ls.tick(:two)]
     sleep rests.look(:two)
   end
 end
